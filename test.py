0adfced327a9feb3d77093094a72be387921e058
Pratik900 <94301043+Pratik900@users.noreply.github.com>
2021-12-22 16:26:02 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,3 @@ string1 = "_Canspirit_"
 string2 = "AI"
 joined_string = string1 +  string2
 print(joined_string)
-heysskkksaka
-x = "hello"
-
-f not type(x) is int:
-  raise TypeError("Only integers are allowed")
